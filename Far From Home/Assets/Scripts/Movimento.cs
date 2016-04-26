6d613c2e8c772c4ee1d667e0ca19346472aaab17
Emiliandro Firmino <emiliandro@live.com>
2016-04-25 22:17:29 -0400

    Criação do projeto Far From Home
    + Criação de pastas
    + Script de Movimento
    + Efeito de câmera (particulas)
    + Assets de prototipação

---
+++ Far From Home/Assets/Scripts/Movimento.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimento : MonoBehaviour {
+
+	[SerializeField] private KeyCode right, left, up, down, run;
+	[SerializeField] private float velright, velleft, o_velright, o_velleft;
+	[SerializeField] private int count;
+	[SerializeField] private GameObject player;
+	[SerializeField] private Rigidbody2D player_rb;
+	[SerializeField] private bool running;
+
+	void Start () {
+		o_velright = velright;
+		count = 0;
+		running = false;
+		StartCoroutine (leftdown ());
+		StartCoroutine (getRB());
+		o_velleft = velleft;
+
+	}
+
+	IEnumerator leftdown(){
+		velleft = velleft * -1;
+		yield return new WaitForSeconds (0);
+		print ("valores corrigidos");
+	}
+
+	IEnumerator getRB() { 
+		player = GameObject.FindGameObjectWithTag ("Player");
+		player_rb = player.GetComponent<Rigidbody2D> ();
+		yield return new WaitForSeconds (3);
+		print ("player is connected");
+	}
+
+	void Update(){
+		if (Input.anyKey) {
+			if (Input.GetKeyDown (right)) count = 1;
+			if (Input.GetKeyDown (left)) count = 2;
+			if (Input.GetKeyDown (up)) count = 3;
+			if (Input.GetKeyDown (down)) count = 4;
+		} else {
+			count = 0;
+		}
+		if (Input.anyKey) {
+			if (Input.GetKeyDown (run)) running = true;
+		} else {
+			running = false;
+		}
+		startMoving ();
+		startRunning();
+	}
+
+	void startMoving (){
+		switch (count) {
+		case 0:
+			player_rb.AddForce (new Vector2 (0,0),ForceMode2D.Impulse);
+			break;
+		case 1:
+			player_rb.AddForce (new Vector2 (velright, 0), ForceMode2D.Impulse);
+			break;
+		case 2:
+			player_rb.AddForce (new Vector2 (velleft,0),ForceMode2D.Impulse);
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		}
+	}
+
+	void startRunning (){
+		switch (running){
+		case true:
+			velleft = o_velleft*1.3f;
+			velright = o_velright*1.3f;
+			break;
+		case false:
+			velleft = o_velleft;
+			velright = o_velright;
+			break;
+		}
+	}
+}
