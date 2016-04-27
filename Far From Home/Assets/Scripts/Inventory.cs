71d217a114f5ded86d88ae07edbb3bd5f4307e07
Emiliandro Firmino <emiliandro@live.com>
2016-04-27 04:06:26 -0400

    +Scenes +Invetario.cs
    Criado base das cenas de menu e vinheta, além do começo do script
    inventario.

---
+++ Far From Home/Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+	public enum items {
+		MOLHO = 1,
+		PEDECABRA = 2,
+		CHAVE = 3
+	}
+	[SerializeField] private items _invetario;
+
+	public void inventario(){
+		switch(_invetario){
+		case items.MOLHO:
+			break;
+		case items.PEDECABRA:
+			break;
+		case items.CHAVE:
+			break; 
+		}
+	}
+}
