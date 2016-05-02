27a0cb183d79f3be168f3b5fee36c60565a56e0c
Emiliandro Firmino <emiliandro@live.com>
2016-05-02 02:17:12 -0400

    Cutscene
    + Cutscene
    - Script que habilita player e item1 bugou, necessita reparos. (cause =
    rg não encontrado em 80% dos testes)

---
+++ Far From Home/Assets/AnimController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimController : MonoBehaviour {
+	[SerializeField] private Animation anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			if (Input.GetKey(KeyCode.LeftShift)) {
+			}
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			if (Input.GetKey(KeyCode.LeftShift)){
+				
+			}
+		}
+		else {
+			
+		}
+	}
+}
