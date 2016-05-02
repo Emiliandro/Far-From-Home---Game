31db8f4055c807f3dd2d630d728b8f8fbe58dd20
RickoBraz <marco.henrique.s.s@gmail.com>
2016-05-02 02:53:53 -0300

    Criando IA do Boss e Script para Camera
    facin facin

---
+++ Far From Home/Assets/Scripts/FollowCamera2D.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera2D : MonoBehaviour {
+	public float interpVelocity, minDistance, followDistance, distanceR, distanceL, floatHeight, liftForce, damping;
+	public GameObject target;
+    public Vector3 offset;
+    Vector3 targetPos;
+
+	// Use this for initialization
+	void Start () {
+		targetPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        RaycastHit2D hit = Physics2D.Raycast(target.transform.position, Vector2.right);
+        if (hit.collider != null)
+        {
+            distanceR = hit.point.x;
+        }
+        if (target){
+			Vector3 posNoZ = transform.position;
+			posNoZ.z = target.transform.position.z;
+		
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+		
+			interpVelocity = targetDirection.magnitude * 5f;
+		
+			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime); 
+		
+			transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.25f);
+		}
+	}
+}
+
---
+++ Far From Home/Assets/Scripts/IaBoss.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IaBoss : MonoBehaviour {
+    [SerializeField] private float velright, velleft;
+    [SerializeField] private int procura;
+    private Rigidbody2D rb;
+    public Vector2 B_Destino;
+
+    // Use this for initialization
+    void Start () {
+        procura = 0;
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(100f, 0f));
+
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Boss1" && procura < 4)
+        {
+            rb.velocity = new Vector2(velleft, 0f);
+            procura += 1;
+        }
+        else if (other.tag == "Boss2" && procura < 3)
+        {
+            rb.velocity = new Vector2(velright, 0f);
+            procura += 1;
+        }
+        else if (other.tag == "Boss2" && procura == 4) TelBoss();
+    }
+    private void TelBoss()
+    {
+        this.transform.position = B_Destino;
+    }
+}
