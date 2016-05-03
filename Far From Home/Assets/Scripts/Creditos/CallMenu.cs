6d73b6bd96e040f009d91da39dce867ec0481003
Emiliandro Firmino <emiliandro@live.com>
2016-05-03 14:47:58 -0400

    Cenas de transição

---
+++ Far From Home/Assets/Scripts/Creditos/CallMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("changeScene",60f);
+	}
+	
+	// Update is called once per frame
+	void changeScene () {
+		Application.LoadLevel("MenuInicial");
+	}
+}
---
+++ Far From Home/Assets/Scripts/Cutscene/GoToGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Update () {
+		Invoke("changeScene",30f);
+	}
+
+	// Update is called once per frame
+	void changeScene () {
+		Application.LoadLevel("tutorial");
+	}
+}
