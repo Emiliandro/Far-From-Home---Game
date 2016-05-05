9b20911a91da2bbd6b3cf7d02597b3d4b6a2e775
Emiliandro Firmino <emiliandro@live.com>
2016-05-05 00:06:24 -0400

    Scene: MenuInicial + Credits adicionados
    bug: KeyCode.DownArrow while moving

---
+++ Far From Home/Assets/Scripts/MenuInicial/MenuI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuI : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] items;
+    private int count;
+	// Use this for initialization
+	void Awake ()
+    {
+        count = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (count == 0)
+        {
+            items[0].transform.localScale = new Vector3(2f, 2f, 2f);
+            items[1].transform.localScale = new Vector3(1f, 1f, 1f);
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                count = 1;
+            } else if (Input.GetKey(KeyCode.DownArrow)) {
+                count = 1;
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                Application.LoadLevel("Cutscene1");
+            }
+        }
+        else {
+            items[1].transform.localScale = new Vector3(2f, 2f, 2f);
+            items[0].transform.localScale = new Vector3(1f, 1f, 1f);
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                count = 0;
+            } else if (Input.GetKey(KeyCode.DownArrow)){
+                count = 0;
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                Application.LoadLevel("Credits");
+            }
+        }
+    }
+}
